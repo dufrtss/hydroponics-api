1f46c75a04639bddd369fc76734cfc77d05055f9
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-04-30 16:35:44 -0300

    changed the no unsafe call suggestion to warn only

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      "@typescript-eslint/no-unsafe-call": "warn",
       'quotes': ['error', 'single', { avoidEscape: true }],
       'semi': ['error', 'never'],
       "indent": ["error", 4],
