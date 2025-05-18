dfb0023c3a67cf8cac709b0bafcf32342bb860aa
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-05-18 16:33:03 -0300

    created env module using a custom env service to derive from config service and allow for seemless env variables access

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env'
+import { envSchema } from './env/env'
 import { AuthModule } from './auth/auth.module'
 import { HttpModule } from './http/http.module'
+import { EnvModule } from './env/env.module'
 
 @Module({
     imports: [
@@ -15,7 +16,8 @@ import { HttpModule } from './http/http.module'
             dbName: 'hydroponics'
         }),
         AuthModule,
-        HttpModule
+        HttpModule,
+        EnvModule
     ]
 })
 export class AppModule {}
