cceec904df74c77d5a087b80aec81c5347a262cd
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-05-18 20:06:40 -0300

    formatted the toDomain mongoose measurement mapper method

--- src/infra/database/mongoose/mappers/mongoose-measurement-mapper.ts
+++ src/infra/database/mongoose/mappers/mongoose-measurement-mapper.ts
@@ -4,11 +4,15 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 export class MongooseMeasurementMapper {
     static toDomain(raw: MeasurementDocument): Measurement {
-        return Measurement.create({
-            sensor: raw.sensor,
-            timestamp: raw.timestamp,
-            data: raw.data,
-        }, new UniqueEntityID(raw._id.toString()))
+        return Measurement.create(
+            {
+                sensor: raw.sensor,
+                timestamp: raw.timestamp,
+                data: raw.data,
+            },
+            new UniqueEntityID(raw._id.toString()
+            )
+        )
     }
 
     static toPersistence(entity: Measurement): MongooseMeasurement {
