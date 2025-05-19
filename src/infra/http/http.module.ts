6dff7ce671273c5b9c732eeb8791f8f31098f6ae
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-05-18 21:46:26 -0300

    provided new use cases to fetch ambient measurements

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -6,15 +6,15 @@ import { AuthenticateAccountController } from './controllers/authenticate-accoun
 import { GetAccountController } from './controllers/get-account.controller'
 import { FetchAccountsController } from './controllers/fetch-accounts.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
-import { GetAmbientTemperatureController } from './controllers/get-ambient-temperature.controller'
-import { GetAmbientHumidityController } from './controllers/get-ambient-humidity.controller'
+import { FetchAmbientTemperatureController } from './controllers/fetch-ambient-temperature.controller'
+import { FetchAmbientHumidityController } from './controllers/fetch-ambient-humidity.controller'
 
 import { AuthenticateAccountUseCase } from '@/domain/account/application/use-cases/authenticate-account'
 import { RegisterAccountUseCase } from '@/domain/account/application/use-cases/register-account'
-import { GetAmbientTemperatureUseCase } from '@/domain/measurement/application/use-cases/get-ambient-temperature'
-import { GetAmbientHumidityUseCase } from '@/domain/measurement/application/use-cases/get-ambient-humidity'
 import { FetchAccountsUseCase } from '@/domain/account/application/use-cases/fetch-accounts'
 import { GetAccountUseCase } from '@/domain/account/application/use-cases/get-account'
+import { FetchAmbientTemperatureUseCase } from '@/domain/measurement/application/use-cases/fetch-ambient-temperature'
+import { FetchAmbientHumidityUseCase } from '@/domain/measurement/application/use-cases/fetch-ambient-humidity'
 
 @Module({
     imports: [
@@ -26,16 +26,16 @@ import { GetAccountUseCase } from '@/domain/account/application/use-cases/get-ac
         GetAccountController,
         FetchAccountsController,
         CreateAccountController,
-        GetAmbientTemperatureController,
-        GetAmbientHumidityController
+        FetchAmbientTemperatureController,
+        FetchAmbientHumidityController
     ],
     providers: [
         AuthenticateAccountUseCase,
         GetAccountUseCase,
         FetchAccountsUseCase,
         RegisterAccountUseCase,
-        GetAmbientTemperatureUseCase,
-        GetAmbientHumidityUseCase
+        FetchAmbientTemperatureUseCase,
+        FetchAmbientHumidityUseCase
     ]
 })
 export class HttpModule {}
